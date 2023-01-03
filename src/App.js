eceb6a832a349f3d6f0ff6152d6335b6d8c1ee5e
alexis avila <evan.avila1@gmail.com>
2023-01-02 17:45:16 -0800

    completed hw

--- src/App.js
+++ src/App.js
@@ -1,45 +1,14 @@
+import Counter from './Counter'
 import './App.css';
-import { useSelector, useDispatch, connect } from 'react-redux'
-import { clearData, fetchData, incrementId, decrementId, inputId } from './features/dataSlice'
-import { useEffect } from 'react';
+import Todo from './Todo'
 
-function App(props) {
-  const dispatch = useDispatch()
-  const data = useSelector((state) => state.data)
-
-  const renderImg = () => {
-    if(data.apiData) {
-      return <img style={{'width': '100vw'}} src={data.apiData.primaryImage} alt={data.apiData.title} />
-    } else {
-      return <p>image here</p>
-    }
-  }
-
-  useEffect(() => {
-    dispatch(fetchData())
-  }, [props.objectId, dispatch])
-
-
+function App() {
   return (
     <div className="App">
-      <div>
-        <button onClick={() => dispatch(fetchData())}>Thunk!</button>
-        <button onClick={() => dispatch(clearData())}>Clear</button>
-        <button onClick={() => dispatch(incrementId())}>Next</button>
-        <button onClick={() => dispatch(decrementId())}>Back</button>
-      </div>
-      <input value={ data.objectId } onChange={(e) => {
-        dispatch(inputId(Number(e.target.value)))
-      }} />
-      <div>
-        {data.objectId}
-        {renderImg()}
-      </div>
-    </div>
+      <Counter />
+      <Todo />
+          </div>
   );
 }
 
-
-const mapStateToProps = (state, ownProps) => ({ objectId: state.data.objectId })
-
-export default connect(mapStateToProps)(App);
+export default App;
